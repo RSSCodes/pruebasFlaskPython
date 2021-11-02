19ea58bfb69593c847cd6051deb2d71a11038937
RSSCodes <rafaelss7770@gmail.com>
2021-11-02 16:39:36 +0000

    agregado de metodos martes.,iercoles,python y concepto

--- principalFlask.py
+++ principalFlask.py
@@ -6,5 +6,27 @@ aplicacion=Flask(__name__)
 def bienvenida():
     return "bienvenido a la pagina"
 
+@aplicacion.route('/martes')
+def metodoMartes():
+    return "es el segundo dia de la semana"
+
+@aplicacion.route('/python')
+def metodoPython():
+    return "es un lenguaje de programacion"
+
+@aplicacion.route('/concepto')
+def conceptoS():
+    elconcepto=request.args.get('laVariable1')
+    if elconcepto=='bit':
+        return "unidad minima de informacion con valores 0 o 1"
+    elif elconcepto=='byte':
+        return "equivale a 8 bits"
+    elif elconcepto=='perrito':
+        return "mejor amigo del nombre"
+    else:
+        return "concepto no encontrado"
+    
+
+
 if __name__=='__main__':
     aplicacion.run(debug=True)
